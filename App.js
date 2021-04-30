d9725eb48a4340d62065fb3120c9eac3b9ec6bb0
Necralma <lihuel.almada@gmail.com>
2021-04-30 12:05:36 -0300

    add menu

--- App.js
+++ App.js
@@ -1,16 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+const Logo = () => <Text>Lalala</Text>
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        title="Ir a detalle"
+        onPress={() => navigation.navigate('Detalle', { user_id: 2})}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+HomeScreen.navigationOptions = {
+  headerTitle: <Logo />,
+  headerStyle: {
+    backgroundColor: '#222',
+  },
+}
+const DestalleScreen = ({ navigation }) => {
+  const lala = navigation.getParam('lala', 'valor por defecto')
+  return (
+    <View style={styles.container}>
+      <Text>Pantalla detalle {lala}</Text>
+      <Button 
+        title="Login"
+        onPress={() => navigation.setParams({ title: 'Usuario 1' })}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+DestalleScreen.navigationOptions = ({ navigation }) => {
+  return {
+    title: navigation.getParam('title', 'Cargando...'),
+  }
+}
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Detalle: {
+    screen: DestalleScreen,
+  }
+}, { 
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#00eecc'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }
+})
+
+export default createAppContainer(AppNavigator)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
